f582d1b09d7ed539c19349182afb8004edca9e43
Amin Faruq <aminfaruk.fa@gmail.com>
2022-11-02 00:14:13 +0800

    Build PokemonModule

---
+++ PokemonModule/PokemonModule/PokemonModule.h
@@ -0,0 +1,18 @@
+//
+//  PokemonModule.h
+//  PokemonModule
+//
+//  Created by Amin faruq on 02/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokemonModule.
+FOUNDATION_EXPORT double PokemonModuleVersionNumber;
+
+//! Project version string for PokemonModule.
+FOUNDATION_EXPORT const unsigned char PokemonModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokemonModule/PublicHeader.h>
+
+
